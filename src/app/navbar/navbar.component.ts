32e8b50762e40ee255e484c6b4472ed4ef779761
Aman Kumar <aman1221997@gmail.com>
2025-09-27 19:06:01 +0530

    Website Intialization

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+// import { AppService } from '../services/app.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  public logo: any;
+  public brandName = "Nipra Oasis";
+  public css: any;
+  public links= [
+    {
+      text: "Home",
+      link: "/home"
+    },
+    {
+      text: "About",
+      link: "/about-us"
+    },
+    {
+      text: "Contact",
+      link: "/contact-us"
+    }
+  ];
+  
+  // Mobile menu properties
+  public isMobileMenuOpen: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+
+  // Mobile menu methods
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    // Prevent body scroll when menu is open
+    if (this.isMobileMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+    document.body.style.overflow = 'auto';
+  }
+}
